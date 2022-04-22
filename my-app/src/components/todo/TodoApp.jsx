69e75dfb3c5333599f0ce81efcb048b1897bb8ba
David Méndez <david.mendez@gtl.net>
2022-04-21 19:15:42 -0500

    31

--- my-app/src/components/todo/TodoApp.jsx
+++ my-app/src/components/todo/TodoApp.jsx
@@ -15,11 +15,43 @@ class TodoApp extends Component {
 
 class LoginComponenet extends Component {
 
+    constructor(props) {
+
+        super(props)
+        this.state = {
+            username: 'in28minutes',
+            password: ''
+        }
+
+        this.handleUserNameChange = this.handleUserNameChange.bind(this)
+        this.handlePasswordChange = this.handlePasswordChange.bind(this)
+
+    }
+
+    handleUserNameChange(event) {
+
+        // console.log(event.target.value);
+        this.setState({
+            username: event.target.value
+        })
+
+    }
+
+
+    handlePasswordChange(event) {
+
+        // console.log(event.target.value)
+        this.setState({
+            password: event.target.value
+        })
+
+    }
+
     render() {
         return (
             <>
-                User Name: <input type="text" name="username"/>
-                Password: <input type="password" name="password"/>
+                User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleUserNameChange} />
+                Password: <input type="password" name="password" value={this.state.password} onChange={this.handlePasswordChange} />
                 <button>Login</button>
             </>
         )
