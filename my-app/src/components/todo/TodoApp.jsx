2d64ec0f6e20961c64122b5532efd1bcea4b9d67
David Méndez <david.mendez@gtl.net>
2022-04-22 06:24:44 -0500

    33

--- my-app/src/components/todo/TodoApp.jsx
+++ my-app/src/components/todo/TodoApp.jsx
@@ -20,10 +20,13 @@ class LoginComponenet extends Component {
         super(props)
         this.state = {
             username: 'in28minutes',
-            password: ''
+            password: '',
+            hasLoginFailed: false,
+            showSuccesfullMsg: false
         }
 
         this.handleChange = this.handleChange.bind(this)
+        this.loginClicked = this.loginClicked.bind(this)
 
     }
 
@@ -36,16 +39,55 @@ class LoginComponenet extends Component {
 
     }
 
+    loginClicked() {
+
+        if(this.state.username === 'in28minutes' && this.state.password === 'dummy') {
+
+            // console.log('Successful')
+            this.setState({showSuccesfullMsg: true, hasLoginFailed: false})
+
+        } else {
+
+            // console.log('Fail')
+            this.setState({showSuccesfullMsg: false, hasLoginFailed: true})
+
+        }
+
+    }
+
     render() {
         return (
             <>
+                <ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed} />
+                <ShowLoginSuccessMsg showLoginSuccessMsg={this.state.showSuccesfullMsg} />
                 User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange} />
                 Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
-                <button>Login</button>
+                <button onClick={this.loginClicked}>Login</button>
             </>
         )
     }
 }
 
+function ShowInvalidCredentials(props) {
+
+    if(props.hasLoginFailed) {
+
+        return <div>Invalid Credentials</div>
+
+    }
+    return null
+
+}
+
+function ShowLoginSuccessMsg(props) {
+
+    if(props.showSuccesfullMsg) {
+
+        return <div>Successful Login</div>
+
+    }
+    return null
+
+}
 
 export default TodoApp;
